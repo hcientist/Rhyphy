fa1f05c326fc6c9a640986aee62e938ab68c94bb
Michael Stewart <tgm@vt.edu>
2023-11-28 15:11:38 -0500

    export the data

--- script/search.js
+++ script/search.js
@@ -10,31 +10,85 @@ const searchForms = [
 
 async function onSearch(ev) {
   // please do not attempt to send the info for me (prevent the default behavior on form submit)
-  ev.preventDefault()
-  console.log(ev)
+  ev.preventDefault();
+  console.log(ev);
   const formData = new FormData(ev.target);
-  console.log('formData', formData)
+  console.log("formData", formData);
   // window.tmp = formData
   // search rhyme api
-  const rhymeResults = await (await searchForRhymes(formData.get('query'))).json() // FIXME
+  const query = formData.get("query");
+  const rhymeResults = await (
+    await searchForRhymes(query)
+  ).json(); // FIXME
 
-  console.log('rhymeResults', rhymeResults)
+  console.log("rhymeResults", rhymeResults);
 
   // with results, do giphy stuff
-
+  const giphURLs = await Promise.all(rhymeResults.map(async (wordObj) => await searchGiphy(wordObj.word)))
+  console.log("giphURLs", giphURLs);
+  const resultSet = persistSearchWithResults(query, rhymeResults, giphURLs);
+  const exportLink = document.getElementById('export')
+  exportLink.href = makeDLURL(resultSet)
+  exportLink.download = true;
 }
 
-function searchGiphy(word) {
+async function searchGiphy(word) {
+  const giphyResp = await fetch(
+    `https://api.giphy.com/v1/gifs/search?api_key=DqDqE5OTJMZkeTKmfCrx287T2jQL1o6t&q=${word}&limit=1&offset=0&rating=r&lang=en&bundle=messaging_non_clips`
+  );
+
+  const giphyResultJSON = await giphyResp.json()
+
+  const resultURL = giphyResultJSON.data[0].images.original.url
+  // console.log('giphy gif url', resultURL)
+  return resultURL
 }
 
 function searchForRhymes(query) {
-  return fetch(`https://rhymebrain.com/talk?function=getRhymes&maxResults=10&word=${query}`)
+  return fetch(
+    `https://rhymebrain.com/talk?function=getRhymes&maxResults=10&word=${query}`
+  );
+}
+
+function makeDLURL (data) {
+  // https://developer.mozilla.org/en-US/docs/Web/API/Blob#creating_a_blob
+  const blob = new Blob([JSON.stringify(data, null, 2)], {
+    type: "application/json",
+  });
+  const blobURL = URL.createObjectURL(blob);
+  return blobURL
+}
+
+function persistSearchWithResults(term, rhymeList, gifList) {
+  console.log("term, rhymeList, gifList", term, rhymeList, gifList);
+
+  const resultSet = resultSetObjFromLists(term, rhymeList, gifList);
+  console.log('resuilt set for local storage', resultSet)
+  localStorage.setItem(term, JSON.stringify(resultSet));
+  return resultSet
+}
+
+function resultSetObjFromLists(word, rhymeList, gifList) {
+  const result = {word, results:[]}
+  rhymeList.forEach((rhymeObj, i) => {
+    let gifResult = gifList[i]
+    result.results.push(makeResultObj(rhymeObj, gifResult))
+  })
+  return result;
+}
+
+function makeResultObj (rhymeResult, gifResult) {
+  return {
+    word: rhymeResult.word,
+    rhymeData: rhymeResult,
+    gifURL: gifResult
+  }
 }
 
 // need to notice the form was submitted
-function addListeners (elem) {
+function addListeners(elem) {
   // given a form element as "elem", add an event listener
-  elem.addEventListener('submit', onSearch)
+  elem.addEventListener("submit", onSearch);
 }
 
-searchForms.forEach(addListeners)
+searchForms.forEach(addListeners);
